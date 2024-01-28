a4527f2617d2309a95a8e0dc44ccfa01ec777706
activexbc <activexbc23@icloud.com>
2024-01-28 23:46:44 +0000

    1

--- app/head.js
+++ app/head.js
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>EveryBodyEATZ</title>
+      <title>EveryBodyEatz</title>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
